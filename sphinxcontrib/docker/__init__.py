488a3e2c5c7c8f3074f7ffa3cc6fdf72802a48aa
Andrew Halper <ashalper@usgs.gov>
2022-03-11 10:18:01 -0700

    Modified cookiecutter template.

--- sphinxcontrib/docker/__init__.py
+++ sphinxcontrib/docker/__init__.py
@@ -4,21 +4,33 @@
 
     A Sphinx extension for documenting Docker files.
 
-    :copyright: Copyright 2017 by Andrew Halper <ashalper@usgs.gov>
-    :license: BSD, see LICENSE for details.
+y    :license: BSD, see LICENSE for details.
 """
 
 import pbr.version
+from docutils import nodes
+from docutils.parsers.rst import Directive
 
 if False:
     # For type annotations
     from typing import Any, Dict  # noqa
     from sphinx.application import Sphinx  # noqa
 
-__version__ = pbr.version.VersionInfo(
-    'docker').version_string()
+__version__ = pbr.version.VersionInfo('sphinxcontrib.docker').version_string()
+
+
+class Docker(Directive):
+
+    def run(self):
+        paragraph_node = nodes.paragraph(text='Hello World!')
+        return [paragraph_node]
 
 
 def setup(app):
-    # type: (Sphinx) -> Dict[unicode, Any]
-    return {'version': __version__, 'parallel_read_safe': True}
+    app.add_directive("helloworld", Docker)
+
+    return {
+        'version': __version__,
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+}
