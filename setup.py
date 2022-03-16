b17f18e04f44c92f006b836e228d69cc717ce40d
Andrew Halper <ashalper@usgs.gov>
2022-03-16 12:25:47 -0700

    setup.py: added extras_require, and "name" to setuptools.setup().

--- setup.py
+++ setup.py
@@ -2,8 +2,16 @@
 
 import setuptools
 
+extras_require = {
+    "markdown": ["myst_parser", "docutils>=0.16"],
+    "docs": [
+        "myst_parser",
+        "docutils>=0.16"
+    ],
+}
 
 setuptools.setup(
+    name="sphinxcontrib-docker",
     setup_requires=['pbr'],
     pbr=True,
 )
